ec2ce3425fd4c96b6b9c460bfe54d861f5c882f9
Giri Kuncoro <girikuncoro@gmail.com>
2016-12-06 15:00:27 -0800

    Add sentiment schema

--- vsenti-database/vsenti_schema.sql
+++ vsenti-database/vsenti_schema.sql
@@ -14,3 +14,33 @@ CREATE TABLE `media` (
     ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Storing product info
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(10) unsigned NOT NULL UNIQUE auto_increment,
+  `name` varchar(255) collate utf8_unicode_ci NOT NULL UNIQUE,
+  `website_url` varchar(255) collate utf8_unicode_ci UNIQUE,
+  `logo_url` varchar(255) collate utf8_unicode_ci UNIQUE,
+  `description` text collate utf8_unicode_ci,
+  `inserted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Storing sentiment categories
+DROP TABLE IF EXISTS `sentiment`;
+CREATE TABLE `sentiment` (
+  `id` int(10) unsigned NOT NULL UNIQUE auto_increment,
+  `product_id` int(10) unsigned,
+  CONSTRAINT `fk_sentiment_product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES product(`id`)
+    ON DELETE CASCADE,
+  `name` varchar(255) collate utf8_unicode_ci NOT NULL UNIQUE,
+  `inserted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
