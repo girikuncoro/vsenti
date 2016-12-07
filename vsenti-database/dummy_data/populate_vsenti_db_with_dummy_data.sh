8267599b1862770f19d584b016c7fa3a3d96bddb
Giri Kuncoro <girikuncoro@gmail.com>
2016-12-07 09:24:06 -0800

    Add vmware product data and dummy script

---
+++ vsenti-database/dummy_data/populate_vsenti_db_with_dummy_data.sh
@@ -0,0 +1,8 @@
+# insert product and sentiment data
+python insert_product_data.py
+
+# generate media data
+python insert_media_data.py
+
+# generate article data and article sentiment
+python generate_article_data.py
